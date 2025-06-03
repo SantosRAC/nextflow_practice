f2d2cadf570d7bae04921b90b494ff11eda3ed85
Renato Santos <renatoacsantos@gmail.com>
2025-06-02 22:25:38 -0300

    Draft of changes in generate correlations

--- bin/generate_correlations_corals.py
+++ bin/generate_correlations_corals.py
@@ -6,8 +6,7 @@ import pandas as pd
 from corals.threads import set_threads_for_external_libraries
 set_threads_for_external_libraries(n_threads=1)
 import numpy as np
-from corals.correlation.full.default import cor_full
-from corals.correlation.utils import derive_pvalues, multiple_test_correction
+from corals.correlation.topk.default import cor_topk
 
 parser = argparse.ArgumentParser(description='Generate correlations with corALS')
 parser.add_argument('--expression_matrix', type=str, dest='expression_file',
@@ -15,7 +14,7 @@ parser.add_argument('--expression_matrix', type=str, dest='expression_file',
                     required=True)
 parser.add_argument('--output_network', type=str, dest='output_network',
                     help='Network file', metavar="network.txt",
-                    required=False)
+                    required=True)
 args = parser.parse_args()
 
 matrix_file = args.expression_file
@@ -23,7 +22,11 @@ output_network = args.output_network
 
 expression_matrix = pd.read_csv(matrix_file, sep="\t", index_col=0)
 expression_matrix_transposed = expression_matrix.transpose()
+expression_matrix_transposed_filtered= expression_matrix_transposed.loc[:, expression_matrix_transposed.sum(axis=0) != 0]
 
-expression_matrix_pearson_result = cor_full(expression_matrix_transposed, correlation_type="pearson")
+gene_names=expression_matrix_transposed_filtered.columns.to_list()
+cor_topk_result = cor_topk(expression_matrix_transposed_filtered, k=0.05, correlation_type="pearson", n_jobs=3)
 
-expression_matrix_pearson_result.to_csv(output_network, sep="\t")
+with open(args.output_network, 'w') as out:
+    for i, j, v in zip(cor_topk_result[1][0], cor_topk_result[1][1], cor_topk_result[0]):
+        out.write(f"{gene_names[i]}\t{gene_names[j]}\t{v}\n")
